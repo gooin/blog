eef462723df0ca3114b2fdc335e3050f2d6cf104
gooin <gooin@outlook.com>
2021-02-22 02:14:19 +0800

    doc: 添加之前写的bilibili视频重命名工具

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,9 @@
 module.exports = {
     someSidebar: {
-        Notes: ['others/edit-chrome-flags-in-files'],
+        Notes: [
+            'others/edit-chrome-flags-in-files',
+            'others/bilibili-video-download-rename'
+        ],
         React: ['react/service-worker-indexeddb-client-save-data'],
         Vim: ['vim/useful-vim-command'],
         Docusaurus: ['doc1'],
